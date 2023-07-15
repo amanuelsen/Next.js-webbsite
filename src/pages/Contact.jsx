471f1cbf0cfa7ead6e6b81862e1c399dcbe96f5d
senai <saniy.cr7@gmail.com>
2023-07-15 12:36:48 +0100

    g

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,13 @@
 import Hero from "./components/hero";
 import Contactform from "./components/Contact";
-const Contact=()=>{
-    return(
-        <div>
-        <Hero heading='Contact' message='Submit the form below for more work and quotes.' />
-        <Contactform/>
+
+const Contact = () => {
+  return (
+    <div>
+      <Hero heading='Contact' message='Submit the form below for more work and quotes.' />
+      <Contactform />
     </div>
-    )
-}
+  );
+};
 
 export default Contact;
